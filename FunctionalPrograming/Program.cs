8f40c564baffc5ece0bfdae0f134f33a3ebe6dec
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-04-28 21:58:30 +0530

    [Start Functional Program Practice Problems]

---
+++ FunctionalPrograming/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FunctionalPrograming
+{
+    public class program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Welcome to functional Program Practice Problems");
+
+
+        }
+    }
+}
